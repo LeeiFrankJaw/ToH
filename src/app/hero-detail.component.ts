09d0bacab89ef64c2d8dffb62f659ddb5ea0cdca
Lei Zhao <LeiZh26@gmail.com>
2017-03-02 16:38:39 +0800

    Finish Http

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -20,6 +20,11 @@ export class HeroDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private location: Location
   ) {}
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
   
   goBack(): void {
     this.location.back();
